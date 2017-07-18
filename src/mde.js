868cb2265f3cc138ef82682dfba21cc9f07b4651
Francis Ashley <fa.repo.dev@gmail.com>
2017-07-18 07:04:03 +0100

    convert tabs from 4 to 2 spaces

--- src/mde.js
+++ src/mde.js
@@ -1,28 +1,28 @@
 (function() {
 
-    'use strict';
+  'use strict';
 
-    const reloadButton = require('./modules/reloadButton.js');
-    const logtray       = require('./modules/logtray.js');
+  const reloadButton = require('./modules/reloadButton.js');
+  const logtray       = require('./modules/logtray.js');
 
-    function MobileDevEnvironment(options) {
-        // Default options
-        options = {
-            group           :  typeof options.group           !== 'undefined'   ?   options.group           :   'global',
-            reload          :  typeof options.reload          !== 'undefined'   ?   options.reload          :   true,
-            hardReload      :  typeof options.hardReload      !== 'undefined'   ?   options.hardReload      :   true,
-            logtray          :  typeof options.logtray          !== 'undefined'   ?   options.logtray         :   true,
-            logErrors       :  typeof options.logErrors       !== 'undefined'   ?   options.logErrors   :   true,
-        };
+  function MobileDevEnvironment(options) {
+    // Default options
+    options = {
+      group           :  typeof options.group           !== 'undefined'   ?   options.group           :   'global',
+      reload          :  typeof options.reload          !== 'undefined'   ?   options.reload          :   true,
+      hardReload      :  typeof options.hardReload      !== 'undefined'   ?   options.hardReload      :   true,
+      logtray          :  typeof options.logtray          !== 'undefined'   ?   options.logtray         :   true,
+      logErrors       :  typeof options.logErrors       !== 'undefined'   ?   options.logErrors   :   true,
+    };
 
-        if (options.reload === true) {
-            this.reload = new reloadButton( { hardReload: options.hardReload } );
-        }
-
-        if (options.logtray === true) {
-            this.logtray = new logtray({reload: options.reload, logErrors: options.logErrors, group: options.group});
-        }
+    if (options.reload === true) {
+      this.reload = new reloadButton( { hardReload: options.hardReload } );
     }
 
-    window.MobileDevEnvironment = MobileDevEnvironment;
+    if (options.logtray === true) {
+      this.logtray = new logtray({reload: options.reload, logErrors: options.logErrors, group: options.group});
+    }
+  }
+
+  window.MobileDevEnvironment = MobileDevEnvironment;
 })();

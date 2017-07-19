e8f5e1fbd14ef58adff5b847805abdb0bed0a67f
Francis Ashley <fa.repo.dev@gmail.com>
2017-07-19 17:57:59 +0100

    remove redundant path to reload element

--- src/modules/logtray.js
+++ src/modules/logtray.js
@@ -18,7 +18,6 @@ module.exports = function logtray(options, DB) {
   self.icon     = { toggleTray: '<svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3v-3h18v3z"/></svg>' };
   self.elements = {
     controlbar: document.querySelector('#mde-controlbar'),
-    reload: document.querySelector('#mde-reload'),
     toggleTray: {},
     tray: {},
     resizeTray: {},

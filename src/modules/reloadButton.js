c50cd584fc583fccb3b3e906fb9400cf82cbc543
mullet <fletcher@prjctnxt.com>
2017-07-19 14:14:41 +0100

    clear up reloadButton.js syntax

--- src/modules/reloadButton.js
+++ src/modules/reloadButton.js
@@ -1,19 +1,20 @@
-function reload(options) {
-  // Libraries
-  const crel = require('crel');
-  // Elements
-  self.elements = { reload: {} };
+module.exports = function reloadButton(options) {
 
-  crel(document.querySelector('#mde-controlbar'),
-    self.elements.reload = crel('button', { 'id': 'mde-reload', 'class': 'mde' } )
-  );
+  'use strict';
+
+  const crel = require('crel'),
+        hardReload = options.hardReload === true;
+  let   button;
+
+  // Create button
+  button = crel('button', { 'id': 'mde-reload' });
 
   // Add svg icon to button
-  self.elements.reload.innerHTML  = '<svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/><path d="M0 0h24v24H0z" fill="none"/></svg>';
+  button.innerHTML  = '<svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/><path d="M0 0h24v24H0z" fill="none"/></svg>';
 
-  self.elements.reload.addEventListener('click', (e) => {
-    location.reload(options.hardReload);
-  }, false);
-}
+  // Listen to button
+  button.addEventListener('click', (e) => location.reload(hardReload));
 
-module.exports = reload;
+  // Add to DOM
+  crel(document.querySelector('#mde-controlbar'), button);
+};

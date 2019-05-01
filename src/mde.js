ffd36e4ed8a1ca52478e7581263113e4f7523bb7
Francis Ashley <fa.repo.dev@gmail.com>
2019-05-01 17:08:15 +0100

    update packages and add npm run scripts

--- src/mde.js
+++ src/mde.js
@@ -1,36 +1,40 @@
 (function() {
-
-  'use strict';
+  "use strict";
 
   function MobileDevEnvironment(options = {}) {
-
     // Default options
     options = {
-      group               :  typeof options.group               !== 'undefined'   ?   options.group                 :   'global',
-      modules             :  typeof options.modules             !== 'undefined'   ?   options.modules               :   ['reload', 'logtray'],
-      controlbarOrder     :  typeof options.controlbarOrder     !== 'undefined'   ?   options.controlbarOrder       :   ['reload', 'logtray'],
-      controlbarPosition  :  typeof options.controlbarPosition  !== 'undefined'   ?   options.controlbarPosition    :   'tr',
-      hardReload          :  typeof options.hardReload          !== 'undefined'   ?   options.hardReload            :   true,
-      displayErrors       :  typeof options.displayErrors       !== 'undefined'   ?   options.displayErrors         :   true,
-      useConsoleLog       :  typeof options.useConsoleLog       !== 'undefined'   ?   options.useConsoleLog         :   false
+      group: typeof options.group !== "undefined" ? options.group : "global",
+      modules: typeof options.modules !== "undefined" ? options.modules : ["reload", "logtray"],
+      controlbarOrder:
+        typeof options.controlbarOrder !== "undefined"
+          ? options.controlbarOrder
+          : ["reload", "logtray"],
+      controlbarPosition:
+        typeof options.controlbarPosition !== "undefined" ? options.controlbarPosition : "tr",
+      hardReload: typeof options.hardReload !== "undefined" ? options.hardReload : true,
+      displayErrors: typeof options.displayErrors !== "undefined" ? options.displayErrors : true,
+      useConsoleLog: typeof options.useConsoleLog !== "undefined" ? options.useConsoleLog : false
     };
 
     // Import modules and tools
-    const reloadButton  = require('./modules/reloadButton.js');
-    const logtray       = require('./modules/logtray.js');
-    const crel          = require('crel');
-    let DB              = require('./tools/db.js');
-    DB                  = new DB(options.group);
+    const reloadButton = require("./modules/reloadButton.js");
+    const logtray = require("./modules/logtray.js");
+    const crel = require("crel");
+    let DB = require("./tools/db.js");
+    DB = new DB(options.group);
 
     // inject control bar into page
-    crel(document.body,
-      crel('div', { 'id': 'mde-controlbar', 'class': 'mde-controlbar-' + options.controlbarPosition } )
+    crel(
+      document.body,
+      crel("div", { id: "mde-controlbar", class: "mde-controlbar-" + options.controlbarPosition })
     );
 
     // Run modules
-    options.controlbarOrder.forEach((module) => {
-      if (module === 'reload' && options.modules.includes('reload')) new reloadButton(options);
-      else if (module === 'logtray' && options.modules.includes('logtray')) new logtray(options, DB);
+    options.controlbarOrder.forEach(module => {
+      if (module === "reload" && options.modules.includes("reload")) new reloadButton(options);
+      else if (module === "logtray" && options.modules.includes("logtray"))
+        new logtray(options, DB);
     });
   }
 
@@ -39,6 +43,6 @@
 
   // Export mde to node
   if (typeof module === "object" && typeof module.exports === "object") {
-      module.exports = MobileDevEnvironment;
+    module.exports = MobileDevEnvironment;
   }
 })();
--- src/modules/reloadButton.js
+++ src/modules/reloadButton.js
@@ -1,20 +1,20 @@
 module.exports = function reloadButton(options) {
+  "use strict";
 
-  'use strict';
-
-  const crel = require('crel'),
-        hardReload = options.hardReload === true;
-  let   button;
+  const crel = require("crel"),
+    hardReload = options.hardReload === true;
+  let button;
 
   // Create button
-  button = crel('button', { 'id': 'mde-reload' });
+  button = crel("button", { id: "mde-reload" });
 
   // Add svg icon to button
-  button.innerHTML  = '<svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/><path d="M0 0h24v24H0z" fill="none"/></svg>';
+  button.innerHTML =
+    '<svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/><path d="M0 0h24v24H0z" fill="none"/></svg>';
 
   // Listen to button
-  button.addEventListener('click', (e) => location.reload(hardReload));
+  button.addEventListener("click", e => location.reload(hardReload));
 
   // Add to DOM
-  crel(document.querySelector('#mde-controlbar'), button);
+  crel(document.querySelector("#mde-controlbar"), button);
 };
